4a1cab578d8ccd778646e35ccdd4c9d0b2f79b25
ripuranjan-143 <ripuranjan143@gmail.com>
2025-07-24 12:50:26 +0530

    create files and folders of all component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import HomePage from './landing_page/home/HomePage'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
